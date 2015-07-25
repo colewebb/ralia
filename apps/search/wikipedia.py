e1c739b4e3daef5a88ee06be44f2dec472ca3d22
colewebb <cole.webb@outlook.com>
2015-07-25 13:36:05 -0700

    added file structure, added searching functionalities

--- apps/search/wikipedia.py
+++ apps/search/wikipedia.py
@@ -1,4 +1,4 @@
 import subprocess
-input = raw_input("What do you want to search for? ")
+input = raw_input("What do you want to search Wikipedia for? ")
 execute = "firefox https://en.wikipedia.org/wiki/Special:Search?search='" + input + "'"
 subprocess.call(execute, shell=True)
---
+++ ralia.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	system("chmod a+x /home/rebooted/Scripts/ralia/ralia.pyc");
+	system("/home/rebooted/Scripts/ralia/ralia.pyc");
+}
